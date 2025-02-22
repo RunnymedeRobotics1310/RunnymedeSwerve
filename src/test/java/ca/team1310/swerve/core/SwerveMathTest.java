f2c31f1966345d37fd10d4a4510eb79ee6de61f0
Jeff Zakrzewski <jeff@zakr.ca>
2025-02-22 13:37:58 -0500

    Revert "Some additional SwerveMath test cases. (NOT ALL PASSING)"
    This reverts commit 5d16891a5c16fc9381c42e045645393367bf1a2e.

--- src/test/java/ca/team1310/swerve/core/SwerveMathTest.java
+++ src/test/java/ca/team1310/swerve/core/SwerveMathTest.java
@@ -4,19 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * @author Tony Field
  * @since 2025-02-19 21:14
  */
 public class SwerveMathTest {
-
-    /**
-     * The epsilon to use for double comparisons. Values off by more than this amount are considered different.
-     */
-    private static final double EPSILON = 1e-5;
 
     private static double trackWidth = .45;
     private static double wheelBase = .65;
@@ -26,27 +19,11 @@ public class SwerveMathTest {
         assertTrue(true, "true is true");
     }
 
-    @ParameterizedTest
-    @CsvSource(
-        {
-            "0.5,    0,   0",
-            "1.0, 0, 0",
-            "0, 1.0, 0",
-            "0.75,    0.15,   0.25",
-            //"1.0, 2.0, 3.0", // fails to demonstrate test parameter validation
-            //"0.0, 0.2, 3.0", // fails to demonstrate test parameter validation
-            "-0.8,  0.05, -0.5",
-        }
-    )
-    public void testToModuleVelocitiesAndBack(double x, double y, double omega) {
-        // validate test parameters
-        double speed = Math.hypot(x, y);
-        assertTrue(
-            speed <= 1,
-            "speed (magnitude of xy vector) must be between -1 and 1 - invalid test parameter - fix the test."
-        );
-        assertTrue(omega <= 1, "omega must be between -1 and 1 - invalid test parameter - fix the test.");
-        assertTrue(omega >= -1, "omega must be between -1 and 1 - invalid test parameter - fix the test.");
+    @Test
+    public void testToModuleVelocitiesAndBack() {
+        double x = 0.5;
+        double y = 0;
+        double omega = 0;
 
         double[] velocities = SwerveMath.calculateModuleVelocities(trackWidth, wheelBase, x, y, omega);
 
@@ -59,6 +36,16 @@ public class SwerveMathTest {
         double brs = velocities[6];
         double bra = velocities[7];
 
+        assertEquals(0.5, frs, "frs");
+        assertEquals(0.5, fls, "fls");
+        assertEquals(0.5, brs, "brs");
+        assertEquals(0.5, bls, "bls");
+
+        assertEquals(0, fra, "fra");
+        assertEquals(0, fla, "fla");
+        assertEquals(0, bra, "bra");
+        assertEquals(0, bla, "bla");
+
         double[] backToRobot = SwerveMath.calculateRobotVelocity(
             trackWidth,
             wheelBase,
@@ -71,74 +58,8 @@ public class SwerveMathTest {
             brs,
             bra
         );
-        assertEquals(x, backToRobot[0], EPSILON, "x component");
-        assertEquals(y, backToRobot[1], EPSILON, "y component");
-        assertEquals(omega, backToRobot[2], EPSILON, "omega component");
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        {
-            "0.5, 0, 0, 0.5, 0, 0.5, 0, 0.5, 0, 0.5, 0",
-            "0,0,0,0,0,0,0,0,0,0,0",
-            "1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0",
-            "0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1",
-        }
-    )
-    public void testToModuleVelocities(
-        double x,
-        double y,
-        double omega,
-        double frs,
-        double fra,
-        double fls,
-        double fla,
-        double bls,
-        double bla,
-        double brs,
-        double bra
-    ) {
-        double[] velocities = SwerveMath.calculateModuleVelocities(trackWidth, wheelBase, x, y, omega);
-
-        assertEquals(frs, velocities[0], EPSILON, "frs");
-        assertEquals(fra, velocities[1], EPSILON, "fra");
-        assertEquals(fls, velocities[2], EPSILON, "fls");
-        assertEquals(fla, velocities[3], EPSILON, "fla");
-        assertEquals(bls, velocities[4], EPSILON, "bls");
-        assertEquals(bla, velocities[5], EPSILON, "bla");
-        assertEquals(brs, velocities[6], EPSILON, "brs");
-        assertEquals(bra, velocities[7], EPSILON, "bra");
-    }
-
-    @ParameterizedTest
-    @CsvSource({ "0.5, 0, 0.5, 0, 0.5, 0, 0.5, 0, 0.5,    0,   0", "0,0,0,0,0,0,0,0,0,0,0" })
-    public void testToRobotVelocity(
-        double frs,
-        double fra,
-        double fls,
-        double fla,
-        double bls,
-        double bla,
-        double brs,
-        double bra,
-        double x,
-        double y,
-        double omega
-    ) {
-        double[] backToRobot = SwerveMath.calculateRobotVelocity(
-            trackWidth,
-            wheelBase,
-            frs,
-            fra,
-            fls,
-            fla,
-            bls,
-            bla,
-            brs,
-            bra
-        );
-        assertEquals(x, backToRobot[0], EPSILON, "x component is as expected");
-        assertEquals(y, backToRobot[1], EPSILON, "y component is as expected");
-        assertEquals(omega, backToRobot[2], EPSILON, "omega component is as expected");
+        assertEquals(x, backToRobot[0], "x component is as expected");
+        assertEquals(y, backToRobot[1], "y component is as expected");
+        assertEquals(omega, backToRobot[2], "omega component is as expected");
     }
 }
