6c6a2f59a2be1378777ed6cc1936e70cb032e04b
Jeff Zakrzewski <jeff@zakr.ca>
2025-10-11 12:00:56 -0400

    Added spotless auto code formatting pre commit check, reformatted all code with google java formatter.

--- src/test/java/ca/team1310/swerve/core/SwerveMathTest.java
+++ src/test/java/ca/team1310/swerve/core/SwerveMathTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
-
 /**
  * @author Tony Field
  * @since 2025-02-19 21:14
